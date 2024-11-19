0bf83c4094fb3ef781fd47043d4a4a5fb945850f
sebastianc94 <sebastian.celsi@gmail.com>
2024-11-18 22:43:59 -0300

    add UsersModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,8 @@ import { AIModule } from './modules/ai/ai.module';
 import { PrismaService } from './prisma/prisma.service';
 import { DislikedMovieModule } from './modules/disliked-movie/disliked-movie.module';
 import { LikedMealModule } from './modules/liked-meal/liked-meal.module';
-import { MovieModule } from './modules/movies/movie.module';
 import { UsersModule } from './modules/users/users.module';
+import { MovieModule } from './modules/movies/movie.module';
 
 @Module({
   imports: [AIModule, DislikedMovieModule, LikedMealModule, MovieModule, UsersModule],
