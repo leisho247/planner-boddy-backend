93f3ffd39592ce1abc2bbc2bae037daa41e0f5f6
sebastianc94 <sebastian.celsi@gmail.com>
2024-11-18 22:11:17 -0300

    add MovieModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AIModule } from './modules/ai/ai.module';
 import { PrismaService } from './prisma/prisma.service';
 import { DislikedMovieModule } from './modules/disliked-movie/disliked-movie.module';
 import { LikedMealModule } from './modules/liked-meal/liked-meal.module';
+import { MovieModule } from './modules/movies/movie.module';
 
 @Module({
-  imports: [AIModule, DislikedMovieModule, LikedMealModule],
+  imports: [AIModule, DislikedMovieModule, LikedMealModule, MovieModule],
   providers: [PrismaService]
 })
 export class AppModule {}
