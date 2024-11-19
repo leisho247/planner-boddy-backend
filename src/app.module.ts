cba6645445997c9f8aaeb94e02984fbdcd21d139
sebastianc94 <sebastian.celsi@gmail.com>
2024-11-18 22:04:54 -0300

    add LikedMealModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AIModule } from './modules/ai/ai.module'; 
 import { PrismaService } from './prisma/prisma.service';
 import { DislikedMovieModule } from './modules/disliked-movie/disliked-movie.module';
+import { LikedMealModule } from './modules/liked-meal/liked-meal.module';
 
 @Module({
-  imports: [AIModule, DislikedMovieModule],
+  imports: [AIModule, DislikedMovieModule, LikedMealModule],
   providers: [PrismaService]
 })
 export class AppModule {}
