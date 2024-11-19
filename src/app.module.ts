71950a3ca171bf9ddc99e8f8775eccacebc76280
sebastianc94 <sebastian.celsi@gmail.com>
2024-11-18 21:44:26 -0300

    add disliked-movie module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AIModule } from './modules/ai/ai.module'; 
+import { PrismaService } from './prisma/prisma.service';
+import { DislikedMovieModule } from './modules/disliked-movie/disliked-movie.module';
 
 @Module({
-  imports: [AIModule],
+  imports: [AIModule, DislikedMovieModule],
+  providers: [PrismaService]
 })
 export class AppModule {}
