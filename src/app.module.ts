6bd5e8dd1ff0cbee5652d3a4a365c8fdce641098
sebastianc94 <sebastian.celsi@gmail.com>
2024-11-18 22:34:20 -0300

    new UsersModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { PrismaService } from './prisma/prisma.service';
 import { DislikedMovieModule } from './modules/disliked-movie/disliked-movie.module';
 import { LikedMealModule } from './modules/liked-meal/liked-meal.module';
 import { MovieModule } from './modules/movies/movie.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [AIModule, DislikedMovieModule, LikedMealModule, MovieModule],
+  imports: [AIModule, DislikedMovieModule, LikedMealModule, MovieModule, UsersModule],
   providers: [PrismaService]
 })
 export class AppModule {}
