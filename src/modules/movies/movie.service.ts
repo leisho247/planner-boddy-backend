459f0290b46ca3d51f3dd352d1a8dc9cea06f039
sebastianc94 <sebastian.celsi@gmail.com>
2024-11-18 22:11:54 -0300

    new movie.service

---
+++ src/modules/movies/movie.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class MovieService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getMovies() {
+    try {
+      const movies = await this.prisma.movie.findMany({
+        select: {
+          id: true,
+          title: true,
+          urlImage: true, // Incluye la imagen si está disponible
+        },
+      });
+
+      return movies;
+    } catch (error) {
+      throw new Error('Error fetching movies');
+    }
+  }
+}
