9b587bc73179b1d5e3bb3450d44d4a3aeaf1b479
sebastianc94 <sebastian.celsi@gmail.com>
2024-11-18 23:03:07 -0300

    add UsersInEvent Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { LikedMealModule } from './modules/liked-meal/liked-meal.module';
 import { UsersModule } from './modules/users/users.module';
 import { MovieModule } from './modules/movies/movie.module';
 import { PlacesModule } from './modules/places/places.module';
+import { UsersInEventModule } from './modules/users-in-event/users-in-event.module';
 
 @Module({
-  imports: [AIModule, DislikedMovieModule, LikedMealModule, MovieModule, UsersModule, PlacesModule],
+  imports: [AIModule, DislikedMovieModule, LikedMealModule, MovieModule, UsersModule, PlacesModule, UsersInEventModule],
   providers: [PrismaService]
 })
 export class AppModule {}
