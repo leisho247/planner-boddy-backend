be7963b30475ae3d96d919951fd7aa7ba51805a3
sebastianc94 <sebastian.celsi@gmail.com>
2024-11-22 22:49:44 -0300

    modified app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma/prisma.service';
 import databaseConfig from './config/database.config';
-
 import { DislikedMealModule } from './modules/disliked-meal/disliked-meal.module';
 import { LikePlaceModule } from './modules/like-place/like-place.module';
 import { LikeMovieModule } from './modules/like-movie/like-movie.module';
