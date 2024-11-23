d0b5524675ea0fbd224b734a3a90d9f212bac66e
sebastianc94 <sebastian.celsi@gmail.com>
2024-11-22 22:17:51 -0300

    change app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AIModule } from './modules/ai/ai.module'; 
 import { PrismaService } from './prisma/prisma.service';
 import { DislikedMovieModule } from './modules/disliked-movie/disliked-movie.module';
 import { LikedMealModule } from './modules/liked-meal/liked-meal.module';
-import { UsersModule } from './modules/users/users.module';
 import { MovieModule } from './modules/movies/movie.module';
 import { PlacesModule } from './modules/places/places.module';
 import { UsersInEventModule } from './modules/users-in-event/users-in-event.module';
+import { ConfigModule } from '@nestjs/config';
+import databaseConfig from './config/database.config';
 
 @Module({
-  imports: [AIModule, DislikedMovieModule, LikedMealModule, MovieModule, UsersModule, PlacesModule, UsersInEventModule],
+  imports: [ 
+  ConfigModule.forRoot({
+    isGlobal: true,
+    load: [databaseConfig],
+  }),
+  DislikedMovieModule, LikedMealModule, MovieModule, PlacesModule, UsersInEventModule],
   providers: [PrismaService]
 })
 export class AppModule {}
